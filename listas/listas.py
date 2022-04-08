1c724f03c03bcf953d7e65c2ccd6d6707cb39ec7
cpina <claudio.i.pina.n@gmail.com>
2022-04-08 14:48:36 +0000

    update list.py

--- listas/listas.py
+++ listas/listas.py
@@ -1,40 +1,61 @@
-class Nodo(object):
+class Node:
+    def __init__(self, data):
+        self.item = data
+        self.ref = None
+
+
+
+class LinkedList:
+    def __init__(self):
+        self.start_node = None
     
-    def __init__(self, dato=None, prox = None):
-        self.dato = dato
-        self.prox = prox
-        
     def __str__(self):
-        return str(self.dato)
+        if self.start_node is None:
+            print("List has no element")
+        else:
+            n = self.start_node
+            message = "["
+            while n is not None:
+                message = message + "'" + str(n.item) + "'" + ", "
+                n = n.ref
+            message = message[:-2] + "]"
+        return message
 
-        
+    
+    def insert_at_end(self, data):
+        new_node = Node(data)
+        if self.start_node is None:
+            self.start_node = new_node
+        else:
+            n = self.start_node
+            while n.ref is not None:
+                n = n.ref
+            n.ref = new_node
+    
 
+                
+    def __len__(self):
+        n = self.start_node
+        cont = 0
+        while n is not None:
+            cont += 1
+            n = n.ref
+        return cont
+            
+    def insert_at_start(self, data):
+        new_node = Node(data)
+        new_node.ref = self.start_node
+        self.start_node = new_node
     
-def verLista(nodo):
-    if nodo.dato == None:
-        return
-    else:
-        while nodo:
-            # muestra el dato
-            print(nodo)
-            # ahora nodo apunta a nodo.prox
-            nodo = nodo.prox
-        
-        
-
-if __name__ == "__main__":
-    a = Nodo()
-    b = Nodo("Datos nodo b")
-    c = Nodo("datos nodo c",b)
-    print("Revisando el nodo 'a'")
-    print(verLista(a))
-    print("")
     
-    print("Revisando el nodo 'b'")
-    print(verLista(b))
-    print("")
-    
-    print("Revisando el nodo 'c'")
-    print(verLista(c))
-    print("")
-    
+if __name__=="__main__":
+    cont=2
+    l = LinkedList() #    l.start_node = None
+    l.insert_at_start("Juaquin Perez")
+    l.insert_at_start("alejandra prieto")
+    l.insert_at_start("emilio moyano")
+    p = ["emilio moyano","alejandra prieto","Juaquin Perez"]
+    #print("con ", cont, ": ",l)
+    print(len(l))
+    print(p)
+    print(l)
