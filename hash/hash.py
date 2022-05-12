e6a5a9618199cb5300b5b3d530130637974211cb
cpina <claudio.i.pina.n@gmail.com>
2022-05-12 12:45:22 -0400

    hash que admite sobrecarga con listas.

--- hash/hash.py
+++ hash/hash.py
@@ -1,52 +1,52 @@
-import numpy as np
-
 class Hash:
-    def __init__(self, largo=100):
+    def __init__(self, largo=10):
         self.largo = largo
-        self.tabla = np.zeros(self.largo)
+        self.tabla = [None]*self.largo
         self.len = 0
     def agregar(self, elemento):
         indice = self._hash_funcion(elemento)
-        if self.tabla[indice] == 0:
-            self.tabla[indice] = elemento
+        if self.tabla[indice] == None:
+            variable = [elemento]
+            self.tabla[indice] = variable.copy()
         else:
-            self.tabla[indice+1] = elemento
+            self.tabla[indice].append(elemento)
         self.len += 1
+      
     def _hash_funcion(self, elemento):
-        indice = elemento % self.largo
-        return indice
+        if type(elemento) is str:
+          indice=len(elemento)
+          indice = indice % self.largo
+          return indice
+        else:  
+          indice = elemento % self.largo
+          indice = round(indice)
+          return indice
+        
         
     def buscar(self, elemento):
         indice = self._hash_funcion(elemento)
         if self.tabla[indice] == elemento:
-            return True
-        if self.tabla[indice+1] == elemento:
             return True
         else:
             return False
         
     def __len__(self):
         return self.len
+
+    def __str__(self):
+      return str(self.tabla)
         
         
         
         
 if __name__ == "__main__":
     variable_hash = Hash()
-    variable_hash.agregar("153")
-    variable_hash.agregar(53)
+    variable_hash.agregar(153)
+    variable_hash.agregar(253)
+    variable_hash.agregar(155)
+    variable_hash.agregar("hola")
     
     print("Los elementos en variable_hash son:",len(variable_hash))
-    if variable_hash.buscar(142):
-        print("Si existe")
-    else:
-        print("No existe")
-    if variable_hash.buscar(153):
-        print("Si existe")
-    else:
-        print("No existe")
-        
-    if variable_hash.buscar(53):
-        print("Si existe")
-    else:
-        print("No existe")
+
+    print("Toda la tabla es:")
+    print(variable_hash)
