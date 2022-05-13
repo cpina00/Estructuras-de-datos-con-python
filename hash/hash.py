7f4808cb7817fc3cb51892b2b87d6c8b24f80038
cpina <claudio.i.pina.n@gmail.com>
2022-05-13 11:09:09 -0400

    Funcion hash modificada, admite todos los tipos básicos, falta arreglar función buscar

--- hash/hash.py
+++ hash/hash.py
@@ -13,15 +13,14 @@ class Hash:
         self.len += 1
       
     def _hash_funcion(self, elemento):
-        if type(elemento) is str:
-          indice=len(elemento)
-          indice = indice % self.largo
-          return indice
-        else:  
-          indice = elemento % self.largo
-          indice = round(indice)
-          return indice
-        
+        elemento = str(elemento)
+        i = 1
+        indice = 0
+        for e in elemento:
+          indice += ord(e)**i
+          i +=1
+        print(elemento, " -> Indice =", indice)
+        return indice % self.largo
         
     def buscar(self, elemento):
         indice = self._hash_funcion(elemento)
@@ -45,6 +44,7 @@ if __name__ == "__main__":
     variable_hash.agregar(253)
     variable_hash.agregar(155)
     variable_hash.agregar("hola")
+    variable_hash.agregar("alguna cosa rara")
     
     print("Los elementos en variable_hash son:",len(variable_hash))
 
