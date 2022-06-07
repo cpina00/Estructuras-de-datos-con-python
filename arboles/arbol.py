2a982f886b340f2b9652a3ea3ea8335dd17f3905
Claudio Piña <claudio.i.pina.n@gmail.com>
2022-06-07 12:44:30 -0400

    Tenemos la función mostrar: ascendente y descendente

--- arboles/arbol.py
+++ arboles/arbol.py
@@ -36,3 +36,50 @@ class Arbol():
                     nodo.izq = new_nodo
                 else:
                     self.agregar_recursivo(nodo.izq, new_nodo)
+
+    def __str__(self):
+        return self.mostrar()
+    
+    def mostrar(self, ascendente = False):
+        if ascendente:
+            return "ascendente: " + self.mostrar_ascendente(self.raiz)
+        else:
+            return "descendente: " + self.mostrar_descendente(self.raiz)
+            
+    
+    def mostrar_descendente(self, nodo):
+        if nodo.data != None and nodo.der != None and nodo.izq != None:
+            return str(self.mostrar_descendente(nodo.izq)) +"->" + str(nodo) +"->"+ str(self.mostrar_descendente(nodo.der))
+        elif nodo.data != None and nodo.izq != None:
+            return str(self.mostrar_descendente(nodo.izq)) +"->"+ str(nodo)
+        elif nodo.data != None and nodo.der != None:
+            return str(nodo) +"->"+ str(self.mostrar_descendente(nodo.der))
+        elif nodo.data != None:
+            return str(nodo)
+    
+    def mostrar_ascendente(self, nodo):
+        if nodo.data != None and nodo.der != None and nodo.izq != None:
+            return str(self.mostrar_ascendente(nodo.der)) +"->" + str(nodo) +"->"+ str(self.mostrar_ascendente(nodo.izq))
+        elif nodo.data != None and nodo.der != None:
+            return  str(self.mostrar_ascendente(nodo.der)) +"->"+  str(nodo)
+        elif nodo.data != None and nodo.izq != None:
+            return str(nodo) +"->"+ str(self.mostrar_ascendente(nodo.izq))
+        elif nodo.data != None:
+            return str(nodo)
+            
+
+if __name__=="__main__":
+    arbol_a = Arbol(5)    
+    
+    arbol_a.agregar(2)
+    arbol_a.agregar(8)
+    arbol_a.agregar(1)
+    arbol_a.agregar(9)
+    arbol_a.agregar(3)
+    arbol_a.agregar(10)
+    arbol_a.agregar(4)
+    print(f"el arbol {arbol_a.mostrar(False)}")
+    print(f"el arbol {arbol_a.mostrar(True)}")
+    
+    
+        
